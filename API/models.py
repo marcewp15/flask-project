faffc65dbb65fe1d37c9948a817bfb03df4afd98
marcewp15 <970@holbertonschool.com>
2021-05-02 21:46:24 -0500

    Adding the flask-project

---
+++ API/models.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    identification = db.Column(db.Integer, unique=True)
+    phone = db.Column(db.Integer)
